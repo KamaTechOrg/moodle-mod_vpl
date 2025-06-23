42b0f42fa4cb390fd6dd5931bd299f07aab12338
michalGit10 <7668744@gmail.com>
2025-06-23 19:44:40 +0300

    adding perf to measure_binding

--- jail/default_scripts/measure_binding.cpp
+++ jail/default_scripts/measure_binding.cpp
@@ -4,6 +4,48 @@
 #include <cstring>
 #include <fcntl.h>
 
+#include <linux/perf_event.h>
+#include <asm/unistd.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <cerrno>
+#include <cstdint>
+#include <iostream>
+
+int perf_event_open(struct perf_event_attr* hw_event, pid_t pid, int cpu, int group_fd, unsigned long flags) {
+    return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+}
+
+int startPerfCounting(pid_t pid) {
+    struct perf_event_attr pe{};
+    memset(&pe, 0, sizeof(struct perf_event_attr));
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    pe.exclude_hv = 1;
+
+    int fd = perf_event_open(&pe, pid, -1, -1, 0);
+    if (fd == -1) {
+        std::cerr << "Error opening perf event: " << strerror(errno) << std::endl;
+        return -1;
+    }
+
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+    return fd;
+}
+
+uint64_t stopPerfCounting(int fd) {
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    uint64_t count = 0;
+    read(fd, &count, sizeof(count));
+    close(fd);
+    return count;
+}
+
+
 namespace py = pybind11;
 
 static std::chrono::high_resolution_clock::time_point start_time;
